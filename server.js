0509154a4d48394e734e64f62a2c242d5df59637
Lukz <lucaseduarte6@gmail.com>
2026-08-08 22:22:00 -0300

    API Docs page review and publish

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const swaggerSpec = require('./src/config/swagger');
 const { apiReference } = require('@scalar/express-api-reference');
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 const isProduction = process.env.NODE_ENV === 'production';
 
